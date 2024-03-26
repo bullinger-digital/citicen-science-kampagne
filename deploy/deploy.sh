2154160cf250266bfce2a7ba67c61e90943dddf9
raphael-m <raaphii@gmail.com>
2024-03-26 12:10:29 +0100

    Add deployment scripts

---
+++ deploy/deploy.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+set -e -u
+
+APP_NAME=citizen-science-app
+STACK_NAME=$APP_NAME
+DEPLOYMENT_TIMEOUT_SECONDS=120
+IMAGE=ghcr.io/bullinger-digital/bullinger-citizen-science:latest
+
+echo "Deploying $APP_NAME..."
+cd citizen-science
+
+echo " * Login to docker..."
+echo "$GITHUB_TOKEN" | docker login ghcr.io -u $ --password-stdin
+
+echo " * Pull images..."
+docker pull "$IMAGE"
+
+# DOCKER INITIALIZATION
+if docker node ls > /dev/null 2>&1; then
+  echo " * Swarm already initialized"
+else
+  echo " * Docker swarm initializing.."
+  docker swarm init
+fi
+
+echo " * Deploying..."
+IMAGE=$IMAGE \
+  docker stack deploy --compose-file docker-compose.yml --prune $STACK_NAME
+
+echo " * Awaiting docker stack... (timeout: $DEPLOYMENT_TIMEOUT_SECONDS seconds)"
+sh docker-stack-wait.sh -t $DEPLOYMENT_TIMEOUT_SECONDS $STACK_NAME
+
+echo " * Docker prune..."
+docker system prune -af
+
+echo " * Logout from docker..."
+docker logout ghcr.io
+
+echo "Deploying $APP_NAME finished."
