ce0a324f7798f37026975cdec7fd35e8d2ee4f72
raphael-m <raaphii@gmail.com>
2024-03-26 15:19:01 +0100

    Add text to front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,10 @@ export default function Home() {
   return (
     <main className="p-5 text-center">
       <p className="mb-4">Willkommen</p>
+      <p>
+        Diese Seite befindet sich im Aufbau. Bitte schauen Sie zu einem späteren
+        Zeitpunkt wieder vorbei.
+      </p>
     </main>
   );
 }
