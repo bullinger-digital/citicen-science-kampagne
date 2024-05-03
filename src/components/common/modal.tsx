fc1cd8cfcc9eeefed419c99663d5f24dc2b7b954
raphael-m <raaphii@gmail.com>
2024-05-03 10:27:20 +0200

    Make modal draggable, improve close

--- src/components/common/modal.tsx
+++ src/components/common/modal.tsx
@@ -1,5 +1,7 @@
 import { ReactNode, useRef } from "react";
 import { createPortal } from "react-dom";
+import Draggable from "react-draggable";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Modal = ({
   open,
@@ -8,6 +10,7 @@ const Modal = ({
   title = "Dialog",
   children,
   maxWidth,
+  closeOnOutsideClick = false,
 }: {
   open: boolean;
   cancel?: () => any;
@@ -15,41 +18,56 @@ const Modal = ({
   title?: ReactNode;
   children: ReactNode;
   maxWidth?: number;
+  closeOnOutsideClick?: boolean;
 }) => {
   const wrapperRef = useRef(null);
   return open
     ? createPortal(
         <div
           ref={wrapperRef}
-          className="fixed top-0 left-0 flex items-start justify-center w-screen h-screen p-10 overflow-auto"
+          className="fixed top-0 left-0 w-screen h-screen p-10 overflow-auto"
           style={{ background: "rgba(100,100,100,0.2)" }}
           onClick={(e) =>
-            e.target === wrapperRef.current && cancel ? cancel() : null
+            e.target === wrapperRef.current && closeOnOutsideClick && cancel
+              ? cancel()
+              : null
           }
         >
-          <div
-            className={`bg-white rounded-lg shadow-2xl w-full`}
-            style={{ minHeight: "100px", maxWidth: maxWidth || 1200 }}
+          <Draggable
+            handle="#drag-handle"
+            defaultPosition={{
+              x: window.outerWidth / 2 - (maxWidth || 1200) / 2,
+              y: 100,
+            }}
           >
-            <header className="p-4 pl-5 text-2xl">{title}</header>
-            <div className="pb-4 pl-5 pr-5">{children}</div>
-            <div className="float-right p-4 pr-5">
-              <button
-                onClick={cancel}
-                className="px-2 py-1 text-white bg-gray-300 rounded-md"
-              >
-                Schliessen
-              </button>
-              {save && (
-                <button
-                  onClick={save}
-                  className="px-2 py-1 ml-1 text-white bg-emerald-600 rounded-md"
+            <div
+              className={`bg-white rounded-lg shadow-2xl w-full`}
+              style={{ minHeight: "100px", maxWidth: maxWidth || 1200 }}
+            >
+              <div className="flex justify-between">
+                <header
+                  id="drag-handle"
+                  className="p-4 pl-5 text-2xl cursor-grab"
                 >
-                  Speichern
+                  {title}
+                </header>
+                <button title="Schliessen" className="p-3" onClick={cancel}>
+                  <AiOutlineClose className="text-3xl text-gray-400 hover:text-gray-600" />
                 </button>
-              )}
+              </div>
+              <div className="pb-2 pl-5 pr-5">{children}</div>
+              <div className="text-right p-4 pr-5">
+                {save && (
+                  <button
+                    onClick={save}
+                    className="px-2 py-1 ml-1 text-white bg-emerald-600 rounded-md"
+                  >
+                    Speichern
+                  </button>
+                )}
+              </div>
             </div>
-          </div>
+          </Draggable>
         </div>,
         document.body
       )
