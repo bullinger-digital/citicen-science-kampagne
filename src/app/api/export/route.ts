3419f53f75b359b1c2f99db96bb9be1f1303c12c
raphael-m <raaphii@gmail.com>
2024-05-27 10:47:08 +0200

    Make import/export routes available to data-admin only

--- src/app/api/export/route.ts
+++ src/app/api/export/route.ts
@@ -3,9 +3,12 @@ import { withRequireRoleAppApi } from "@/lib/security/withRequireRole";
 
 import { NextRequest } from "next/server";
 
-export const GET = withRequireRoleAppApi("admin", async (req: NextRequest) => {
-  await exportToCurrentCommit();
-  return Response.redirect(
-    "https://github.com/bullinger-digital/bullinger-korpus-tei/compare/citizen-science-experiments?expand=1"
-  );
-});
+export const GET = withRequireRoleAppApi(
+  "data-admin",
+  async (req: NextRequest) => {
+    await exportToCurrentCommit();
+    return Response.redirect(
+      "https://github.com/bullinger-digital/bullinger-korpus-tei/compare/citizen-science-experiments?expand=1"
+    );
+  }
+);
--- src/app/api/import/route.ts
+++ src/app/api/import/route.ts
@@ -7,10 +7,13 @@ import { withRequireRoleAppApi } from "@/lib/security/withRequireRole";
 
 import { NextRequest } from "next/server";
 
-export const GET = withRequireRoleAppApi("admin", async (req: NextRequest) => {
-  console.log("Route called: importing from repository");
-  await initRepository();
-  //await pullRepository();
-  await importFromCurrentCommit();
-  return new Response("Import done!");
-});
+export const GET = withRequireRoleAppApi(
+  "data-admin",
+  async (req: NextRequest) => {
+    console.log("Route called: importing from repository");
+    await initRepository();
+    //await pullRepository();
+    await importFromCurrentCommit();
+    return new Response("Import done!");
+  }
+);
