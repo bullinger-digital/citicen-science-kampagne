c93a9afec2ea695401b423e7c1d92beeb75e1032
raphael-m <raaphii@gmail.com>
2024-05-28 18:15:42 +0200

    Handle wrong letter ids

--- src/app/(app)/letter/[letterId]/page.tsx
+++ src/app/(app)/letter/[letterId]/page.tsx
@@ -3,16 +3,15 @@
 import { Editor } from "@/components/editor/editor";
 import { useParams } from "next/navigation";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0/client";
-import { Metadata } from "next";
 
 export default withPageAuthRequired(function LetterPage() {
   const params = useParams<{ letterId: string }>();
 
-  if (isNaN(parseInt(params.letterId))) {
+  if (!params?.letterId || isNaN(parseInt(params.letterId))) {
     return (
       <div className="text-center p-5">
         <h2 className="font-bold">Fehler</h2>
-        Es ist ein Fehler aufgetreten: &quot;{params.letterId}&quot; ist keine
+        Es ist ein Fehler aufgetreten: &quot;{params?.letterId}&quot; ist keine
         gültige Brief-ID.
       </div>
     );
