a626f7958fe9f36dccf02884bbb8cb472ad6b0f3
raphael-m <raaphii@gmail.com>
2024-05-31 16:22:26 +0200

    Add statistics

---
+++ src/lib/actions/stats.ts
@@ -0,0 +1,80 @@
+"use server";
+import "server-only";
+import { kdb } from "@/lib/db";
+import { whereCurrent } from "../versioning";
+import { cache } from "react";
+import { sql } from "kysely";
+
+export const getLetterStats = cache(async () => {
+  const letterStats = await kdb
+    .selectFrom("letter_version")
+    .where(whereCurrent)
+    // Filter out automatic transcriptions
+    // Todo: reuse code from citizen.ts
+    .where((e) =>
+      e.or([
+        e("extract_source", "like", "HBBW-%"),
+        e("extract_source", "like", "TUSTEP-%"),
+      ])
+    )
+    .groupBy("extract_status")
+    .select("extract_status")
+    .select((e) => e.fn.countAll<string>().as("count"))
+    .execute();
+
+  return letterStats;
+});
+
+export const getUserStats = cache(async () => {
+  console.log("Get user stats called (should be cached)");
+  const query = kdb
+    .with("users_counts", (eb) =>
+      eb
+        .selectFrom("user")
+        .select("id")
+        .select((e) =>
+          e
+            .selectFrom("letter_version")
+            .leftJoin("log", "log.id", "letter_version.created_log_id")
+            .where("log.created_by_id", "=", e.ref("user.id"))
+            .where("letter_version.review_state", "<>", "rejected")
+            .select((e) =>
+              e.fn
+                .coalesce((e2) =>
+                  e2.fn.sum<number>(sql`jsonb_array_length(actions)`)
+                )
+                .as("actions_count")
+            )
+            .as("actions_count")
+        )
+        .select((e) =>
+          e
+            .selectFrom("letter_version")
+            .leftJoin("log", "log.id", "letter_version.created_log_id")
+            .where("log.created_by_id", "=", e.ref("user.id"))
+            .where("letter_version.review_state", "<>", "rejected")
+            .where(
+              "letter_version.actions",
+              "@>",
+              `[{ "nodePath": [{ "nodeName": "revisionDesc" }], "attributes": { "status": "finished" } }]`
+            )
+            .select((e) => e.fn.countAll<number>().as("letters_finished_count"))
+            .as("letters_finished_count")
+        )
+    )
+    .selectFrom("users_counts")
+    .select(
+      sql<number>`coalesce(actions_count + (4 * letters_finished_count), 0)`.as(
+        "points"
+      )
+    )
+    .select("users_counts.actions_count")
+    .select("users_counts.letters_finished_count")
+    .select("id")
+    .orderBy("points", "desc")
+    .orderBy("users_counts.id", "asc")
+    .limit(100);
+
+  const userStats = await query.execute();
+  return userStats;
+});
