7d9d995fbd0605e1f066b5a53e1a2f9949256a2e
raphael-m <raaphii@gmail.com>
2024-06-07 09:17:47 +0200

    Add hostname to log file path

--- src/lib/logger/logger.ts
+++ src/lib/logger/logger.ts
@@ -1,7 +1,8 @@
 import pino from "pino";
 import path from "path";
+import os from "os";
 
-const dest = path.join(process.cwd(), "pino-logs/log.log");
+const dest = path.join(process.cwd(), `pino-logs/log-${os.hostname()}.log`);
 console.log("Pino destination: ", dest);
 
 export default pino({
