5b9394391b29ec3da8c70d07a2034a242fe3e054
raphael-m <raaphii@gmail.com>
2024-06-10 12:05:43 +0200

    Add indexes on versioned tables to speed up Admin UI

---
+++ migrations/scripts/2024-06-10T08:50:50.415Z-index_versioned_id.ts
@@ -0,0 +1,18 @@
+import { Kysely } from "kysely";
+
+const versioned_tables = ["letter", "person", "person_alias", "place"];
+
+export async function up(db: Kysely<any>): Promise<void> {
+  // Add index on review_state column to all versioned tables
+  for (const table of versioned_tables) {
+    await db.schema
+      .createIndex(`${table}_id`)
+      .on(`${table}_version`)
+      .columns(["id"])
+      .execute();
+  }
+}
+
+export async function down(db: Kysely<any>): Promise<void> {
+  throw new Error("Down migration not implemented");
+}
